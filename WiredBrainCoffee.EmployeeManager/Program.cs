3cb60e54e36a0edaf957cc540f73e365e2e649c1
Andrew Morgan <andrew.morgan@e.org>
2023-11-27 21:24:45 +0000

    Added employee table

--- WiredBrainCoffee.EmployeeManager/Program.cs
+++ WiredBrainCoffee.EmployeeManager/Program.cs
@@ -1,13 +1,31 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using WiredBrainCoffee.EmployeeManager.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<EmployeeManagerDbContext>(
+    opt => opt.UseSqlServer(
+        builder.Configuration.GetConnectionString("EmployeeManagerDb")));
 
 var app = builder.Build();
+
+await EnsureDatabaseIsMigrated(app.Services);
+
+async Task EnsureDatabaseIsMigrated(IServiceProvider services)
+{
+    using var scope = services.CreateScope();
+    using var ctx = scope.ServiceProvider.GetService<EmployeeManagerDbContext>();
+    if(ctx is not null)
+    {
+        await ctx.Database.MigrateAsync();
+    }
+    
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
