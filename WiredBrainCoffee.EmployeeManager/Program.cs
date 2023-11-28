25cb78f806a005ecdcf0bd4090835e5de9d8f3a2
Andrew Morgan <andrew.morgan@e.org>
2023-11-28 23:10:26 +0000

    Added Employee overview and implemented IDbContextFactory

--- WiredBrainCoffee.EmployeeManager/Program.cs
+++ WiredBrainCoffee.EmployeeManager/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddDbContext<EmployeeManagerDbContext>(
+builder.Services.AddDbContextFactory<EmployeeManagerDbContext>(
     opt => opt.UseSqlServer(
         builder.Configuration.GetConnectionString("EmployeeManagerDb")));
 
